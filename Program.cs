c84426974be14bd1d85652bc56ca2a401b4cac41
Takishita Noriyuki <n.taki.0416@gmail.com>
2019-11-07 07:29:04 +0900

    【FizzBuzz問題の応用編】やや複雑な条件分岐の文法確認

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace FizzBuzzCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 1; i <= 150; i++) {
+                string text = String.Empty;
+                if (i < 100) {
+                    if (i % 3 == 0 ||
+                        String.Format("{0}", i).Contains("3")) {
+                        text = String.Format("{0:00!}", i);
+                    } else {
+                        text = String.Format("{0:000}", i);
+                    }
+                } else {
+                    text = String.Format("{0:000}", i);
+                }
+                Console.WriteLine(text);
+            }
+        }
+    }
+}
